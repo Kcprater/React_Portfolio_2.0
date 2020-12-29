053dde4c844fc333fc94fecbcbb3948ed4020a77
kcprater <kristopherprater@gmail.com>
2020-12-29 12:16:28 -0500

    Updates

---
+++ precache-manifest.969774d06221e3e0071eb298c94bd449.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bafc48464c4d50f0a7bec6a4745f8fd",
+    "url": "/React_Portfolio_2.0/index.html"
+  },
+  {
+    "revision": "cf0bdc7251aa45a718e4",
+    "url": "/React_Portfolio_2.0/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "9f7685691e1de3d46a15",
+    "url": "/React_Portfolio_2.0/static/css/main.f487c217.chunk.css"
+  },
+  {
+    "revision": "cf0bdc7251aa45a718e4",
+    "url": "/React_Portfolio_2.0/static/js/2.9c4b41ca.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/React_Portfolio_2.0/static/js/2.9c4b41ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f7685691e1de3d46a15",
+    "url": "/React_Portfolio_2.0/static/js/main.7b2bde1d.chunk.js"
+  },
+  {
+    "revision": "cb9886ba9f0a5add9a26",
+    "url": "/React_Portfolio_2.0/static/js/runtime-main.a15e0e6c.js"
+  },
+  {
+    "revision": "d3792490af29929846828fc65e720161",
+    "url": "/React_Portfolio_2.0/static/media/HelpingHand.d3792490.png"
+  },
+  {
+    "revision": "863645a8736a7f08804b1f288a7ab07c",
+    "url": "/React_Portfolio_2.0/static/media/KrisPrater.863645a8.jpg"
+  },
+  {
+    "revision": "ee43abb8b8e455f68f937f97f23f9225",
+    "url": "/React_Portfolio_2.0/static/media/KristopherPrater.ee43abb8.pdf"
+  },
+  {
+    "revision": "d44113bcd871dada61d2315e96d46255",
+    "url": "/React_Portfolio_2.0/static/media/background.d44113bc.jpg"
+  },
+  {
+    "revision": "f4ac3c58db02058ff38357561f5e000c",
+    "url": "/React_Portfolio_2.0/static/media/cssMario.f4ac3c58.png"
+  },
+  {
+    "revision": "3194d4a6032edb70aa1171d107da44bc",
+    "url": "/React_Portfolio_2.0/static/media/cssMickey.3194d4a6.png"
+  },
+  {
+    "revision": "ca383a3ad2ec04f60e713f87cb23fffb",
+    "url": "/React_Portfolio_2.0/static/media/efaStatic.ca383a3a.png"
+  },
+  {
+    "revision": "4bacc8108cf8d1a6cbfbce92d900c35f",
+    "url": "/React_Portfolio_2.0/static/media/hpAPI.4bacc810.png"
+  },
+  {
+    "revision": "af227950e3dab38618cbc4d764fde9e4",
+    "url": "/React_Portfolio_2.0/static/media/huluStatic.af227950.png"
+  },
+  {
+    "revision": "11c794975ec26cc7e813ff14dd515412",
+    "url": "/React_Portfolio_2.0/static/media/redBadgeFinal.11c79497.png"
+  },
+  {
+    "revision": "db39ec654aab55bd844c29389f7ed5b6",
+    "url": "/React_Portfolio_2.0/static/media/universalAPI.db39ec65.png"
+  }
+]);
